b40be192b8a635bbcdedf8a60dfec1c4a22dd6cc
Daniel <danieruone@gmail.com>
2021-05-21 21:27:22 -0500

    folder names

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+import font from "assets/fonts/NetflixSansRegular.woff";
+
+export const GlobalStyles = createGlobalStyle`
+    @font-face {
+        font-family: "Netflix Sans Regular";
+        src: url(${font});
+    }
+
+    html,
+    body {
+        font-family: "Netflix Sans Regular";
+        margin: 0;
+    }
+`;
