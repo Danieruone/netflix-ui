dcbf1dac555c4896d81667796166968fa7590c22
Daniel <danieruone@gmail.com>
2021-06-21 16:17:24 -0500

    netflix content and favicon

--- src/content.js
+++ src/content.js
@@ -4,27 +4,27 @@ export const content = [
     photos: [
       {
         photo:
-          "https://occ-0-1595-3934.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABZvbw253rjdf-lDxQgElGKjKa38YquVLfdyRu6L3XZnaunq4Inc5LXyH_KBfD6d7esLp09zMYWv_LvCFygghMWzB_LFiIxGr2tlSW5vMo97FUcZTe9BXBs7s_P-S.jpg?r=c25",
+          "https://occ-0-1595-3934.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABTnF4m3NiYKX4M3b6pdSLe2U1s8C9jrwdCcvl8mlOZtDtmaJTQaEhzpzuxFGx9ZH0Yc-GJB1qplEN3rcOFa8gjxbgNAE3KhuJZTCK0azVe3yqOr4RxaAh085PqJH.jpg?r=f3c",
       },
       {
         photo:
-          "https://occ-0-1595-3934.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABUBVKWQPIGVucB5a77EjB51iZRWNMQe2-7tH8CzKK8bQ0gVMsRVoEv1WGjoS7ZohB55u7XeafSlVy26-29-5ugm93dxddc1QF-n6k38p-BiMIHHUEcRek5k8o8IE.jpg?r=d39",
+          "https://occ-0-1595-3934.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABeTmv9zLH94RC0xWdGFfQwFF9oKI40fK4sua_dh8JunKjogVs7Dk6BQ1TpSJwO2UHD2ajMnPfhqwNTTT2qIy7gzpphKdi-Ei32s1nx7jsFqYEALtbfG0_GpPa_m8.jpg?r=d48",
       },
       {
         photo:
-          "https://occ-0-1595-3934.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABdPpxhsaUHsopyRPokj1j3257Ez-DHKtpqD8gLrHAL-uQVRQaDbsakwj5N82Z7jgoZl4PTk9wmdWg3kpt0OCDlILDTaNjQXR1SHx1UaHqkVyP7l8yT2GySqn8Xlz.jpg?r=9b4",
+          "https://occ-0-1595-3934.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABXil1GVMBmZfWRDgv8Oq1dOqlgvKLItmtpchvwEPGoU2ThaQrR_ytmfb1lyRysgFUISTo6UOqUNQeF2NYL5_-5KtHI7l5EeOF4uoyWdYou8NJjMm3vMEFhnaRyGT.jpg?r=a6f",
       },
       {
         photo:
-          "https://occ-0-1595-3934.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABWiP-UYCft_m3gUehu8qoVaOBt74DgzDIl2uIoYRDVN3E3HcPcHNtsg_KU5_-k9GChhRb2-DyKO6sj7U3TTOqo9doko.webp?r=36d",
+          "https://occ-0-1595-3934.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABXMzNMytl1rgMcETQt1Ptp5lvcMyNPr2scEFTfRT-GPxanBwdx11_gzJx6PX5zbQE1CDvZp7aapw7TmO76Tog2HOxpg.webp?r=be8",
       },
       {
         photo:
-          "https://occ-0-1595-3934.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABebMKLEOLI0hRQt2QwEG1MIPa95E-qSLhkq8j7iJyLegtJ0dNN0RE1-Vp7K4lQf1SI_OzOxyrQgEhFxWENdTxrA2YL8.webp?r=c4d",
+          "https://occ-0-1595-3934.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABUe-BFn_Ve6qdd6HPd-gSjnRAyvK31HxxdT2ZzhVcgnaMX6emYSElAblHb4D110qWF_Ft8TNyLe-mIqzTdJTSeFTCIBWj93AbiE2vfK71RaZjx61vcThXGeqJvo_.jpg?r=9d2",
       },
       {
         photo:
-          "https://occ-0-1595-3934.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABSu7qlODscVIiTcqP9zrHXFVuGv9wlljb1u2fYEPwL7L7MLY4EWm6l3iqV9cE1mPPRTugo0ZG26rUp0qZHDvYaot8fQ.webp?r=843",
+          "https://occ-0-1595-3934.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABdyIKCALt8doaPyVH1yxdPPduigEhnoiv1kC57dhbk7ELqRtNgj_dn5KpDj1_7V1LQV31Xfm3JfEY-Cz_a4-oQT5YNA.webp?r=6d5",
       },
     ],
   },
@@ -62,26 +62,34 @@ export const content = [
 export const notifications = [
   {
     photo:
-      "https://occ-0-1595-3934.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABZvbw253rjdf-lDxQgElGKjKa38YquVLfdyRu6L3XZnaunq4Inc5LXyH_KBfD6d7esLp09zMYWv_LvCFygghMWzB_LFiIxGr2tlSW5vMo97FUcZTe9BXBs7s_P-S.jpg?r=c25",
+      "https://occ-0-1595-3934.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABbUbmIPrLMAgm1SpHnfw7fr-NYGOfodgbUxaLABKSm-3HijWpNpCU9_KpVwthca3ZFVMDOFsfWuHuuorpgoRT4o.jpg?r=2ac",
   },
   {
     photo:
-      "https://occ-0-1595-3934.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABUBVKWQPIGVucB5a77EjB51iZRWNMQe2-7tH8CzKK8bQ0gVMsRVoEv1WGjoS7ZohB55u7XeafSlVy26-29-5ugm93dxddc1QF-n6k38p-BiMIHHUEcRek5k8o8IE.jpg?r=d39",
+      "https://occ-0-1595-3934.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABYNSKq8TCpUyIjFufmrh1g8DD14By5vQvcghlTXcT-rw6vEuvxZLRI_BKSCSVdzkZp8JWqpCRcILZq55Q1MEo7A.jpg?r=656",
   },
   {
     photo:
-      "https://occ-0-1595-3934.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABdPpxhsaUHsopyRPokj1j3257Ez-DHKtpqD8gLrHAL-uQVRQaDbsakwj5N82Z7jgoZl4PTk9wmdWg3kpt0OCDlILDTaNjQXR1SHx1UaHqkVyP7l8yT2GySqn8Xlz.jpg?r=9b4",
+      "https://occ-0-1595-3934.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABdz1ZgWwwRpdCf_V6U0YhK_9Ax0jGxH6maS6hAvZl9tk59TFL3NSBeL_kgtKxJkz7dNnABJGYevAomD3FvxcAP8.jpg?r=734",
   },
   {
     photo:
-      "https://occ-0-1595-3934.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABWiP-UYCft_m3gUehu8qoVaOBt74DgzDIl2uIoYRDVN3E3HcPcHNtsg_KU5_-k9GChhRb2-DyKO6sj7U3TTOqo9doko.webp?r=36d",
+      "https://occ-0-1595-3934.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABVNAC4N0rQBWd8DwIU4CrHzIBz3e5oNpfccEKwWsV1O4k3Q9R5qyZKupx-UWCU9YHwVJWvlkJD-vU1pE_ZPU2DM.jpg?r=951",
   },
   {
     photo:
-      "https://occ-0-1595-3934.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABebMKLEOLI0hRQt2QwEG1MIPa95E-qSLhkq8j7iJyLegtJ0dNN0RE1-Vp7K4lQf1SI_OzOxyrQgEhFxWENdTxrA2YL8.webp?r=c4d",
+      "https://occ-0-1595-3934.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABT1Ey3U7gaFJTfOGb7l4mmwnIiGGFwrPvMVqsAjfFIlc6jd4N_GttGFt-fdZ1FZTT1bWCEgHwTXwuoZhN4FRvHA.jpg?r=d02",
   },
   {
     photo:
-      "https://occ-0-1595-3934.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABSu7qlODscVIiTcqP9zrHXFVuGv9wlljb1u2fYEPwL7L7MLY4EWm6l3iqV9cE1mPPRTugo0ZG26rUp0qZHDvYaot8fQ.webp?r=843",
+      "https://occ-0-1595-3934.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABWZ-70W3RlawGLVgnnp6xrZ-8pnFQYulto8025M38ss1aiEpbk4Q0kBSmglGc2hQi1PylFWLOjELnLhilt9Kc_o.jpg?r=1db",
+  },
+  {
+    photo:
+      "https://occ-0-1595-3934.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABXL9LlA-b2tAYMM_dFGmZo88cnsrin51xhk3IxG7KwyYND1efwAYG6wyt2sh8ThTWJS1ARmhr-47l1skmyVcH3c.jpg?r=730",
+  },
+  {
+    photo:
+      "https://occ-0-1595-3934.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABfhQOGJSOlyhXoV-3T1OtkGNDsQu5-rC87lo7KzRiaiPy66jknQYiZrSxmW1ZoduaXyAvAlGri9bbppuTR8Ai84.jpg?r=f06",
   },
 ];
